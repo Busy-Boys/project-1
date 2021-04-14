6a49e84bcdca17c3f190ad5d1f3bbbf2cb60d7f3
Dave Holst <daveholst@gmail.com>
2021-04-14 18:00:43 +0800

    limited search results to 6

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -20,8 +20,13 @@ function searchForMovies(title) {
   });
 }
 
+// builds the search results container
 function searchResultsBuilder(results) {
-  const searchResults = results;
+  let searchResults = results;
+  // slice search results to six
+  if (searchResults.length > 6) {
+    searchResults = searchResults.slice(0, 6);
+  }
   const mainContainer = document.querySelector('#main');
 
   // append results container to main section element
@@ -35,7 +40,7 @@ function searchResultsBuilder(results) {
   mainContainer.prepend(searchResultsContainer);
 
   // loop results and generate thumbnails
-  results.forEach((result) => {
+  searchResults.forEach((result) => {
     // target element
     const searchResultsContainer = document.querySelector('#search-results');
     // create div
@@ -55,11 +60,6 @@ function searchResultsBuilder(results) {
   });
 }
 
-// TEST EXAMPLE
-// searchForMovies('matrix').then((searchResults) =>
-//   searchResultsBuilder(searchResults)
-// );
-
 //TEST event listen
 searchButton.addEventListener('click', () => {
   const title = searchInput.value;
@@ -67,4 +67,5 @@ searchButton.addEventListener('click', () => {
   searchForMovies(title).then((searchResults) =>
     searchResultsBuilder(searchResults)
   );
+  // .catch(())
 });
