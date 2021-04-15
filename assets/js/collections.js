903da357508de8bf428b010998eff0d378b9ee52
Ritz GL <maurigarzonlara@hotmail.com>
2021-04-15 20:09:30 +0800

    created empty js file with pseudocode comments

---
+++ assets/js/collections.js
@@ -0,0 +1,7 @@
+// file to add movies from search results into collection
+
+// get imdb id from button elements on the DOM
+// pass to function that fetches omdb movie information
+// create object from information received
+// push the element into an array
+// store the array into local storage so it's visible
