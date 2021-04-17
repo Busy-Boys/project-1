b13e6d32edac9aa5627ace9441e2ca729ba4f32e
Ritz GL <maurigarzonlara@hotmail.com>
2021-04-17 19:49:25 +0800

    adding movie to collection, and saving to localStorage w/o duplicates

--- assets/js/collections.js
+++ assets/js/collections.js
@@ -2,7 +2,11 @@
 // pass to function that fetches omdb movie information
 let imdbID = 'tt1285016';
 
-let collection = [];
+if (localStorage.getItem('Collection') === null) {
+  // write a blank array in, otherwise push wont work later.
+  let collection = [];
+  localStorage.setItem('Collection', JSON.stringify(collection));
+}
 
 let selectedFilm = {
   title: '',
@@ -15,14 +19,29 @@ let selectedFilm = {
   posterUrl: '',
   contentRating: '',
   runtime: '',
+  imdbID: '',
 };
 
 function getFilmInformation(imdbID) {
+  // get array from local
+  let collection = localStorage.getItem('Collection');
+  let isDuplicate = false;
+  collection = JSON.parse(collection);
+  // checking to for duplicate imdbID inside collection
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i].imdbID === imdbID) {
+      isDuplicate = true;
+      break;
+    }
+  }
+  // early return to prevent api request from executing based on isDuplicate
+  if (isDuplicate === true) {
+    return;
+  }
   fetch(`${omdbUrl}i=${imdbID}`)
     .then((response) => response.json())
     .then((data) => {
       // copying relevant information from received data will go here
-      console.log('searched information:', data);
       selectedFilm.title = data.Title;
       selectedFilm.year = data.Year;
       selectedFilm.imdbRating = data.imdbRating;
@@ -33,18 +52,13 @@ function getFilmInformation(imdbID) {
       selectedFilm.posterUrl = data.Poster;
       selectedFilm.contentRating = data.Rated;
       selectedFilm.runtime = data.Runtime;
+      selectedFilm.imdbID = imdbID;
+    })
+    .then(() => {
       // store into collection array
       collection.push(selectedFilm);
       // store into localStorage
       localStorage.setItem('Collection', JSON.stringify(collection));
-    })
-    .then(() => {
-      // console log information to test our object has been successfully stored
-      console.log('In js environment:', collection);
-      // in localStorage, needs parsing into java object in order to be usable
-      // as everything is stored as a JSON string of text
-      let storedCollection = localStorage.getItem('Collection');
-      console.log('As stored object:', storedCollection);
     });
 }
 
