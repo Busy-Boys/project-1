9aa994b8e80328454bc938fd4ab42bc6d63e67fb
Dave Holst <daveholst@gmail.com>
2021-04-18 19:30:19 +0800

    CA10: DomBuilder pushing compare card to DOM

--- assets/js/stage.js
+++ assets/js/stage.js
@@ -20,7 +20,58 @@ function stageCardDomBuilder(imdb, side) {
   // check which side the card is going to and assign target Element
   let targetElement = '';
   if (stageSide === 'left') {
+    targetElement = document.querySelector('#stage-card-left');
+  } else {
+    targetElement = document.querySelector('#stage-card-right');
   }
+  // start building card with card div
+  let newCard = document.createElement('div');
+  newCard.classList.add('card');
+  // add the inner html
+  newCard.innerHTML = `
+  <div class="card-content">
+  <div class="media mb-1">
+    <div class="media-left">
+      <figure class="movie-poster image is-128x128">
+        <img
+          src="${selectedFilm.posterUrl}"
+          alt="Movie Poster"
+        />
+      </figure>
+    </div>
+    <div class="media-content">
+      <p class="title is-4 is-family-monospace">${selectedFilm.title}</p>
+      <p class="subtitle is-5 mb-0 is-family-monospace">
+        <b>Year: </b>${selectedFilm.year}
+      </p>
+      <p class="subtitle is-6 mb-0 is-family-monospace">
+        <b>Rating: </b>${selectedFilm.contentRating}
+      </p>
+      <p class="subtitle is-6 mb-0 is-family-monospace">
+        <b>Runtime: </b>${selectedFilm.runtime}
+      </p>
+      <p class="subtitle is-6 mb-0 is-family-monospace">
+        <b>Director: </b>${selectedFilm.director}
+      </p>
+      <p class="subtitle is-6 mb-0 is-family-monospace">
+        <b>IMDB Ratting: </b>${selectedFilm.imdbRating}
+      </p>
+      <p class="subtitle is-6 mb-0 is-family-monospace">
+        <b>Rotten Tomatoes : </b>${selectedFilm.rottenRating}
+      </p>
+      <p class="subtitle is-6 mb-0 is-family-monospace">
+        <b>Metacritic : </b>${selectedFilm.metacriticRating}
+      </p>
+    </div>
+  </div>
+
+  <div class="content">
+    <p>${selectedFilm.shortPlot}</p>
+  </div>
+</div>
+  `;
+  // append to target container
+  targetElement.appendChild(newCard);
 }
 
 stageCardDomBuilder('tt0068646', 'left');
